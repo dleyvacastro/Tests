730d37976aa2f6db038f876f556b8be5a5c9d8f7
dleyvacastro <dleyvacastro@gmail.com>
2021-12-17 00:21:56 -0500

    autoinstall script

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,5 +1,10 @@
 sudo sh ./dep.sh
 sh ./dep_yay.sh
+git clone https://github.com/dleyvacastro/Tests.git
+cp -r ./Tests/kitty ~/.config
+cp -r ./Tests/fish ~/.config
 cd ~/.config
 git clone https://github.com/dleyvacastro/qtile.git
+git clone https://github.com/dleyvacastro/Neovim.git
+mv Neovim nvim
 
